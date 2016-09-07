c4eeda0c5e4bf4144e7c54da4b885a0a240aea4b
martin.balch <martybalch21@live.com>
2016-09-07 12:02:13 -0700

    need to work on this shit

--- Pongish/main.cpp
+++ Pongish/main.cpp
@@ -5,6 +5,11 @@
 
 using namespace sfw;
 
+//void randomDirection()
+//{
+//	direction = ((rand() % 6) + 1);
+//}
+
 struct vec2
 {
 	float x;
@@ -23,6 +28,7 @@ struct Ball
 	float xPos = 300;
 	float yPos = 100;
 	float size = 30;
+	
 	vec2 direction;
 };
 
@@ -35,11 +41,22 @@ struct Bounds
 
 void drawPaddle(float xPos, float yPos)
 {
-	drawLine(xPos - 50, yPos, xPos + 50, yPos, WHITE);
-	drawLine(xPos - 50, yPos - 30, xPos + 50, yPos - 30, WHITE);
-	drawLine(xPos - 50, yPos, xPos - 50, yPos - 30, WHITE);
-	drawLine(xPos + 50, yPos, xPos + 50, yPos - 30, WHITE);
+	drawLine(xPos - 50, yPos, xPos + 50, yPos, YELLOW);
+	drawLine(xPos - 50, yPos - 30, xPos + 50, yPos - 30, GREEN);
+	drawLine(xPos - 50, yPos, xPos - 50, yPos - 30, GREEN);
+	drawLine(xPos + 50, yPos, xPos + 50, yPos - 30, GREEN);
 }
+
+//Ball createball(float xpos, float ypos, float radius, unsigned int color, float xvel, float yvel)
+//{
+//	Ball retval;
+//	retval.xPos = xpos;
+//	retval.yPos = ypos;
+//	retval.size = radius;
+//
+//
+//	
+//}
 
 
 
@@ -51,15 +68,11 @@ void main()
 	sfw::initContext(800, 600, "NSFW Draw");
 	setBackgroundColor(BLACK);
 	
-	unsigned f = sfw::loadTextureMap("./res/tonc_font.png", 16, 6);
-	unsigned d = sfw::loadTextureMap("./res/fontmap.png", 16, 16);
-	unsigned r = sfw::loadTextureMap("./res/background.jpg");
-	unsigned u = sfw::loadTextureMap("./res/crosshair.png");
-
+	
 	float acc = 0;
 	char c = '\0';
 
-	//sfw::setBackgroundColor(YELLOW);
+	
 
 	while (sfw::stepContext())
 	{
@@ -86,10 +99,7 @@ void main()
 		acc += sfw::getDeltaTime();
 		
 
-		if (sfw::getMouseButton(MOUSE_BUTTON_RIGHT))
-			sfw::drawTexture(u, sfw::getMouseX(), sfw::getMouseY(), sfw::getTextureWidth(u) / 2.5, sfw::getTextureHeight(u) / 2.5, 45, true, 0, 0x88ffffff);
-		else sfw::drawTexture(u, sfw::getMouseX(), sfw::getMouseY(), sfw::getTextureWidth(u) / 2, sfw::getTextureHeight(u) / 2);
-
+		
 		
 	}
 
